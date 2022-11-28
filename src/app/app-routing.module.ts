6f36a024536aa675af75a6a7f13264a0e31f4e3c
Newborshian <christopherbouthemy2014@gmail.com>
2022-11-28 11:08:14 +0100

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { Login2Component } from './login2/login2.component';
 import { ProductsComponent } from './products/products.component';
 import { RegisterComponent } from './register/register.component';
+import { Register2Component } from './register2/register2.component';
 
 const routes: Routes = [
   { path: 'homepage', component: HomepageComponent},
   { path: 'products', component: ProductsComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
+  { path: 'register2', component: Register2Component},
+  { path: 'login2', component: Login2Component},
 ];
 
 @NgModule({
