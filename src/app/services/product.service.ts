bc2c9198c92bdc2017e2d5a3ca0e566220ff6449
Baschounette <steph.basch77@gmail.com>
2022-11-30 20:36:35 +0100

    typage

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +11,11 @@ export class ProductService {
   constructor(private http:HttpClient) { }
 
   //Récupère tout les champions de la BDD
-  getAllProductFromService(){
-    return this.http.get('http://localhost:8080/products/allProduct')
+  getAllProductFromService(): Observable<Product[]> {
+    return this.http.get('http://localhost:8080/products/allProduct') as Observable<Product[]>
   }
   
-  addProductFromService(product: any){
+  addProductFromService(product: Product){
     return this.http.post('http://localhost:8080/products/addProduct', product)
   }
 }
