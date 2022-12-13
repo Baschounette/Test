ee6ed3aadaf3d6b392b79f843a647b28c2693da7
Baschounette <steph.basch77@gmail.com>
2022-12-13 14:56:29 +0100

    ...

--- src/app/components/register3/register3.component.ts
+++ src/app/components/register3/register3.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register3',
   templateUrl: './register3.component.html',
   styleUrls: ['./register3.component.css']
 })
-export class Register3Component {
+export class Register3Component implements OnInit {
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    
+  }
 
 }
