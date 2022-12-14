d28bdcdc119d7631fe2f9b6c1c0f95c650aef42c
Newborshian <christopherbouthemy2014@gmail.com>
2022-12-14 13:46:27 +0100

    ...

--- src/app/components/Basch/login2/login2.component.ts
+++ src/app/components/Basch/login2/login2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, resolveForwardRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 
@@ -15,7 +16,7 @@ export class Login2Component implements OnInit {
   public loginNotValidate = false;
 
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
       
@@ -25,14 +26,19 @@ export class Login2Component implements OnInit {
     this.userService.loginFromService(user).subscribe((res) => {
       console.log(res)
       if(res == 1) {
-        console.log("connexion réussi :D")
-        this.loginGood = true;
+        this.router.navigate(["homepage"])
       } else if(res == 2) {
         console.log("Pas encore validé, dommage :'(")
         this.loginNotValidate = true;
+        setTimeout(() => {
+          this.loginNotValidate = false
+        }, 2500);
       } else {
         console.log("Identifiant incorrect :/")
         this.loginFail = true;
+        setTimeout(() => {
+          this.loginFail = false
+        }, 2500);
       }
     })
   }
