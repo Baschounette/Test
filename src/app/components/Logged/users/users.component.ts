e19e27c15ec3cf3794a6db74dd43ae94d485206d
Newborshian <christopherbouthemy2014@gmail.com>
2022-12-21 00:40:10 +0100

    ...

--- src/app/components/Logged/users/users.component.ts
+++ src/app/components/Logged/users/users.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  
+  public users: User[] | undefined;
 
+  constructor(private userService: UserService) { }
+  
+  ngOnInit(): void {
+    this.getAllUser();
+  }
+
+  getAllUser(){
+    this.userService.getAllUserFromService().subscribe((res: User[]) => {
+      this.users =res;
+    })
+  }
 }
