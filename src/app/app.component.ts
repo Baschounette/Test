dd86451e87603b2f8c88cb22b7ed03d738d2ce61
Baschounette <steph.basch77@gmail.com>
2023-01-05 11:37:30 +0100

    456465

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+
   title = 'Test';
+
+  ngOnDestroy(): void {
+    localStorage.removeItem("Name")
+    localStorage.removeItem("Role")
+    localStorage.removeItem("Connecté ?")
+  }
+
 }
